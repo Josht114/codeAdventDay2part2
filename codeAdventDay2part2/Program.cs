4dabfe56e08232c18b015e6f1084ed8f7bd855a1
Josh <josht114@hotmail.com>
2018-12-04 21:18:03 +0000

    basic shell setup

--- codeAdventDay2part2/Program.cs
+++ codeAdventDay2part2/Program.cs
@@ -13,9 +13,9 @@ namespace codeAdventDay2part2
         {
             List<String> listOfBoxIds = new List<String>();
             int count = 0;
-            int twoMatchCount = 0;
-            int threeMatchCount = 0;
-            int checksum = 0;
+
+            string finalBoxOne = "";
+            string finalBoxTwo = "";
 
             while (true) // Loop indefinitely
             {
@@ -35,158 +35,47 @@ namespace codeAdventDay2part2
 
                     foreach (string box in listOfBoxIds)
                     {
-
-                        char[] letters = box.ToCharArray();
-
-
-                        if (getMatchCount(letters, 2))
+                        foreach (string boxTwo in listOfBoxIds)
                         {
-                            twoMatchCount++;
-                            string boxString = new string(letters);
-                            Console.Write("2 match- " + boxString + Environment.NewLine);
-                        }
-                        else
-                        {
-                            string boxString = new string(letters);
-                            Console.Write(" 2 FAILED - " + boxString + Environment.NewLine);
+                            char[] lettersBoxOne = box.ToCharArray();
+                            char[] lettersBoxTwo = boxTwo.ToCharArray();
+
+                            if (getCloseMatch(lettersBoxOne, lettersBoxTwo))
+                            {
+                                finalBoxOne = box;
+                                finalBoxTwo = boxTwo;
+                            }
+
                         }
 
-                        if (getMatchCount(letters, 3))
-                        {
-                            threeMatchCount++;
-                            string boxString = new string(letters);
-                            Console.Write("3 match- " + boxString + Environment.NewLine);
-                        }
-                        else
-                        {
-                            string boxString = new string(letters);
-                            Console.Write("3 FAILED - " + boxString + Environment.NewLine);
-                        }
-                        Console.Write(Environment.NewLine);
+
+
                     }
+                    string same = findSame(finalBoxOne, finalBoxTwo);
 
-                    checksum = threeMatchCount * twoMatchCount;
-
-                    Console.Write("box count " + listOfBoxIds.Count + Environment.NewLine);
-                    Console.Write("2 count " + twoMatchCount + Environment.NewLine);
-                    Console.Write("3 count " + threeMatchCount + Environment.NewLine);
-                    Console.Write("checksum " + checksum + Environment.NewLine);
+                    Console.Write("box 1 " + finalBoxOne + Environment.NewLine);
+                    Console.Write("box 2 " + finalBoxTwo + Environment.NewLine);
+                    Console.Write("same letters " + same + Environment.NewLine);
 
                 }
 
-
             }
 
-
-
-
-
         }
-
-
-        public static bool getMatchCount(Array chars, int matchNumber)
+        public static bool getCloseMatch(Array boxOneChars, Array boxTwoChars)
         {
 
 
-            bool finalOnlyTwo = false;
-            bool finalOnlyThree = false;
-
-
-
-            foreach (char c in chars)
-            {
-                int i = 0;
-
-                bool onlyTwo = false;
-                bool onlyThree = false;
-
-                int matchCount = 0;
-
-                while (i < chars.Length)
-                {
-
-
-                    if (c.Equals(chars.GetValue(i)))
-                    {
-                        matchCount++;
-
-                        // THE PROBLEM, IGNORES A 2 IF IT FINDS A THREE
-
-                        if (matchCount == 2)
-                        {
-                            onlyTwo = true;
-                        }
-
-                        if (matchCount == 3)
-                        {
-                            onlyTwo = false;
-                            onlyThree = true;
-                        }
-
-                        if (matchCount > 3)
-                        {
-                            onlyTwo = false;
-                            onlyThree = false;
-                        }
-
-                    }
-                    i++;
-
-                }
-
-                if (onlyTwo)
-                {
-                    finalOnlyTwo = onlyTwo;
-                }
-
-                if (onlyThree)
-                {
-                    finalOnlyThree = onlyThree;
-                }
-
-            }
-
-            if (matchNumber == 2)
-            {
-                return finalOnlyTwo;
-            }
-
-            if (matchNumber == 3)
-            {
-                return finalOnlyThree;
-            }
 
             return false;
-
         }
 
 
 
-        public static bool getMatchCountforThree(Array chars, int count)
+        public static string findSame(string one, string two)
         {
-            int matchCount = 0;
 
-            int i = 0;
-
-            foreach (char c in chars)
-            {
-
-
-                if (c.Equals(chars.GetValue(i)))
-                {
-                    matchCount++;
-
-                    if (matchCount == 3 && count == 3)
-                    {
-                        return true;
-                    }
-
-
-                }
-                i++;
-            }
-            i = 0;
-            return false;
+            return "";
         }
 
 
